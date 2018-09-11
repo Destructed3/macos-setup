81975b2d66f9b0009c2740bd3dceafe4c29f62af
Hannes Hunzinger <hunzinger@localhero.de>
2018-09-11 10:13:02 +0200

    hopefully corrected sourcing of .zshrc +some updates to echoed hints

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-echo Install Homebrew
+echo ===Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo Install Tools
-brew install git bash-completion mariadb cask
-
+echo ===Install Tools
+brew install git zsh bash-completion mariadb cask
+echo ===Install Casks
 brew cask install \
     caskroom/versions/firefox-developer-edition \
     opera vivaldi google-chrome \
@@ -12,24 +12,27 @@ brew cask install \
     cyberduck filezilla sequel-pro \
     adobe-acrobat-reader libreoffice shiftit keepassxc skype
 
-echo Cleanup MacOS PHP
+echo ===Cleanup MacOS PHP
 brew remove --force --ignore-dependencies httpd
 brew remove --force --ignore-dependencies php70-xdebug php71-xdebug
 brew remove --force --ignore-dependencies php70-imagick php71-imagick
 brew remove --ignore-dependencies --force php70 php71 php72
 
-echo Install PHP via Homebrew
+echo ===Install PHP via Homebrew
 brew install httpd php72 composer
 brew install php72 --with-thttpd --with-thread-safety
 
-echo Installing ValetPlus
+echo ===Install ValetPlus
 composer global require weprovide/valet-plus
-source .bash_profile 'export PATH="$PATH:$HOME/.composer/vendor/bin"'
 
-echo Starting Homebrew Services...
+echo ===Update zsh to use composer
+echo 'export PATH="$PATH:$HOME/.composer/vendor/bin"' >> ~/.zshrc 
+echo source ~/.zshrc
+
+echo ===Start Homebrew Services
 brew services start mariadb
 
-echo installing OhMyZsh
+echo ===Install OhMyZsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Script will exit into zsh
